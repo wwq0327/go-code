60e2860e6d3fd70250884ebddc5bc8284d5b20a4
wwq0327 <wwq0327@gmail.com>
2014-03-29 22:45:25 +0800

    go

--- human.go
+++ human.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
+import "strconv"
 
 type Human struct {
 	name string
@@ -7,12 +8,17 @@ type Human struct {
 }
 
 type Student struct {
-	Human
+	Human // 匿名字段
 	number int
+}
+
+func (s Student) String() string {
+	return "<" + s.name + "-" + strconv.Itoa(s.age) + "-" + strconv.Itoa(s.number) + ">"
 }
 
 func main() {
 	s := Student{Human{"wyatt", 33}, 97019}
 	fmt.Println("this name: ", s.name)
 	fmt.Println("this human name:", s.Human.name)
+	fmt.Println("ALL INFO", s)
 }
