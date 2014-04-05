294c7f756154bec9c414b0100cee9ff5456ec0b4
wwq0327 <wwq0327@gmail.com>
2014-04-05 20:34:09 +0800

    f

--- PIG/option.go
+++ PIG/option.go
@@ -15,40 +15,74 @@ type OptionCommon struct {
 }
 
 type IntOption struct {
-	OptionCommon
-	Value, Min, Max int
+	OptionCommon        // 匿名字段（嵌入）
+	Value, Min, Max int // 具名字段（聚合）
 }
 
 type FloatOption struct {
-	Optioner
-	Value float64
+	Optioner         // 匿名字段，接口嵌入，需要具体的类型, 满足这个接口的都可以接入
+	Value    float64 // 具名字段（聚合）
+}
+
+type StringOption struct {
+	OptionCommon
+	Value string
 }
 
 type GenericOption struct {
 	OptionCommon
 }
 
-
 func (option IntOption) Name() string {
-
+	return name(option.ShortName, option.LongName)
 }
 
 func (option IntOption) IsValid() bool {
+	return option.Min <= option.Value && option.Value <= option.Max
+}
 
+func (option StringOption) Name() string {
+	return name(option.ShortName, option.LongName)
+}
+
+func (option StringOption) IsValid() bool {
+	if option.Value != "" {
+		return true
+	}
+	return false
 }
 
 func name(shortName, longName string) string {
-
+	if longName == "" {
+		return shortName
+	}
+	return longName
 }
 
 func (option GenericOption) Name() string {
-
+	return name(option.ShortName, option.LongName)
 }
 
 func (option GenericOption) IsValid() bool {
-
+	return true
 }
 
 func main() {
+	fileOption := StringOption{OptionCommon{"f", "file"}, "index.html"}
+	topOption := IntOption{OptionCommon: OptionCommon{"t", "top"}, Max: 100}
+	floatOption := FloatOption{GenericOption{OptionCommon{"s", "size"}}, 19.5}
+	float2 := FloatOption{StringOption{OptionCommon{"F", "File"}, "post.html"}, 12.2}
 
+	for _, option := range []Optioner{topOption, fileOption, floatOption, float2} {
+		fmt.Print("name=", option.Name(), "--valid=", option.IsValid(), "--value=")
+
+		switch option := option.(type) {
+		case IntOption:
+			fmt.Println(option.Value, "--Min=", option.Min, "--Max=", option.Max)
+		case StringOption:
+			fmt.Println(option.Value)
+		case FloatOption:
+			fmt.Println(option.Value)
+		}
+	}
 }
