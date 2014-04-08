081fc7905defedb292bb8b7d77126d063294b4b8
wwq0327 <wwq0327@gmail.com>
2014-04-08 15:17:16 +0800

    f

---
+++ pkg/os/r2w.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("w.txt")
+	checkError(err)
+
+	defer f.Close()
+
+	f.WriteString("hello, world\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+}
