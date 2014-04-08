669fdeefb38d15ca1e31a95c53fa1f48dea70742
wwq0327 <wwq0327@gmail.com>
2014-04-08 09:27:20 +0800

    f

---
+++ pkg/os/os3.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// 改变文件模式
+	chmod()
+	// 改变工作目录
+	chdir()
+}
+
+func chdir() {
+	// Chdir
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("The current directory is: %s\n", pwd)
+
+	if err = os.Chdir("/Users/wyatt/golang/go-code/pkg"); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	pwd, err = os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Now the current directory is: %s\n", pwd)
+}
+
+func chmod() {
+	// 	test.txt's mode is: -rw-r--r--(644)
+	// test.txt's mode is: -rwxrwxrwx(777)
+	fi, err := os.Stat("test.txt")
+	if err != nil {
+		fmt.Printf("Error :%s\n", err)
+		return
+	}
+
+	fmt.Printf("test.txt's mode is: %s(%o)\n", fi.Mode(), fi.Mode()&0777)
+
+	err = os.Chmod("test.txt", 0777)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+
+	fi, err = os.Stat("test.txt")
+	if err != nil {
+		fmt.Printf("Error :%s\n", err)
+		return
+	}
+
+	fmt.Printf("test.txt's mode is: %s(%o)\n", fi.Mode(), fi.Mode()&0777)
+
+}
