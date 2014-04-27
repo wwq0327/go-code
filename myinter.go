208e6744d0a933bce97585eab8bc0b399bf0f9c8
wwq0327 <wwq0327@gmail.com>
2014-04-27 22:46:17 +0800

    f

---
+++ myinter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Humaner interface {
+	Say()
+}
+
+type Human struct {
+	Name string
+}
+
+func (h Human) Say() {
+	fmt.Println(h.Name)
+}
+
+func main() {
+	m := Human{"wyatt"}
+	var h Humaner
+	h = m
+	h.Say()
+
+}
